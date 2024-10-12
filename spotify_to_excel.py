a444fe930e34bf0310111a7c4ac64e46af872905
akerraps <akerraps@gmail.com>
2024-10-12 19:43:35 +0200

    Split global functions

--- spotify_to_excel.py
+++ spotify_to_excel.py
@@ -1,66 +1,11 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import pandas as pd
+from get_api import get_tracks
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-OUTPUT = os.getenv('OUTPUT')
 PLAYLIST_ID = os.getenv('PLAYLIST_ID')
-
-try:
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id= CLIENT_ID, client_secret= CLIENT_SECRET))
-except spotipy.oauth2.SpotifyOauthError as e:
-    if "No client_id" in str(e):
-        print("No client ID")
-        exit(1)
-    elif "No client_secret" in str(e):
-        print ("No client secret")
-        exit(1)
-    else:
-        print(f"Spotify Error: {str(e)}")
-        exit(1)
-
-
-def get_tracks(id):
-    try:
-        playlist_info = spotify.playlist( "https://open.spotify.com/playlist/" + id)
-    except spotipy.exceptions.SpotifyException as e:
-        if 'Invalid base62 id' in str(e):
-            print(f"Error: Invalid playlist ID: {id}")
-            exit(1)
-        else:
-            print(f"Spotify Error: {str(e)}")
-            exit(1)
-
-    tracks = playlist_info['tracks']
-
-    all_tracks = tracks['items']
-    while tracks['next']:
-        tracks = spotify.next(tracks)  # Actualizamos tracks con la siguiente página
-        all_tracks.extend(tracks['items'])  # Añadimos los nuevos items a la lista all_tracks
-
-    track_data = []
-
-    for item in all_tracks:
-        track = item['track']
-        track_info = {
-            'Song Name': track['name'],
-            'Artist': track['artists'][0]['name'],  # Primer artista
-            'Album': track['album']['name'],
-            'Duration (ms)': track['duration_ms'],
-            'Popularity': track['popularity'],
-            'Added At': item['added_at']  # Cuándo fue añadida a la playlist
-        }
-        track_data.append(track_info)
-
-    # Crear un DataFrame a partir de la lista de diccionarios
-    df = pd.DataFrame(track_data)
-
-    return df
+OUTPUT = os.getenv('OUTPUT')
 
 def main():
     playlist_id =PLAYLIST_ID
